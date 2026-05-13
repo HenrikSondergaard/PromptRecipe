97534113f45d501b9a8cda2e299d1fe2210e7f53
HenrikSondergaard <77335331+HenrikSondergaard@users.noreply.github.com>
2026-05-13 11:50:12 +0200

    Implement RecipeForm component (Step 1) (#11)
    Also registers RecipeService in DI via Program.cs update.
    
    Closes #3

--- Program.cs
+++ Program.cs
@@ -1,9 +1,12 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using PromptRecipe;
+using PromptRecipe.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddTransient<RecipeService>();
 
 await builder.Build().RunAsync();
